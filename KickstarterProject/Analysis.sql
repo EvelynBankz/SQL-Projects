d4965b44b2ad7dec8e66a65ef9cd2ecb14cd7ed6
Evelyn Bankz <113942245+EvelynBankz@users.noreply.github.com>
2025-04-14 12:16:19 +0100

    Update Analysis SQL

--- KickstarterProject/Analysis.sql
+++ KickstarterProject/Analysis.sql
@@ -1,1 +1,45 @@
+--  Analysis --
+-- Getting the number of projects in each state --
+select  state, count(state) as status
+from 2018starter 
+group by state;
+
+-- Getting the number of backers per category --
+select category, count(backers) as Investors
+from 2018starter
+group by category;
+
+-- Total number of categories --
+select count(distinct(category)) as totalNumberOfCategories
+from 2018starter;
+
+-- Getting the total number of projects in each category --
+select category, count(name) as projects
+from 2018starter
+group by category;
+
+-- Backers per Country --
+select country, count(backers) as countryBackers
+from 2018starter 
+group by country;
+
+-- Total pledgedrealusd per country --
+select country, round(sum(usd_pledged_real), 0) as totalPledgedUSD
+from 2018starter
+group by country;
+
+
+-- Total number of countries --
+select count(distinct(country)) as totalNumberOfCountries
+from 2018starter;
+
+-- Number of projects per country --
+select distinct(country), count(distinct(name)) as totalNumberOfProjects
+from 2018starter
+group by country;
+
+-- Number of country per category --
+select distinct(category), count(distinct(country)) as totalNumberOfCountries
+from 2018starter
+group by category;
 
