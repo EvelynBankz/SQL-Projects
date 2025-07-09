000d16e4a1fd7e0e7c23abbfeb0f4274884f4346
Evelyn Bankz <113942245+EvelynBankz@users.noreply.github.com>
2025-07-09 14:16:47 +0100

    Update Data preparation & ETL.sql

--- KickstarterProject/Datapreparation&ETL.sql
+++ KickstarterProject/Datapreparation&ETL.sql
@@ -1,25 +1,27 @@
 -- Kickstarter Project Data Preparatiin and cleaning --
+
 -- 1. Creating the datababse and the table and the column headings with their datatype --
-CREATE DATABASE starter;
+create database starter;
 use starter;
 create table `2018starter` (ID int, name text, category text, main_category text, currency text,
 deadline text, goal text, launched text, pledged text, state text, backers text, country text, 
 `usd pledged` text, usd_pledged_real text, usd_goal_real text);
 
 -- 2.  importing the table 2018starter csv file --
-SET GLOBAL LOCAL_INFILE=ON;
-LOAD DATA LOCAL INFILE 'C:/KickStarterDataset/ks-projects-201801.csv'
-INTO TABLE `2018starter`
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 ROWS;
+set global local_infile=on;
+load data local infile '/Users/user/Downloads/ks-projects-201801.csv'
+into table `2018starter`
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\r\n'
+ignore 1 rows;
 
 -- 3. Finding Incorrect Values in each columns --
 -- A. Incorrect Values in ID column --
 -- should return empty if there are no text values in the column --
 select * from 2018starter
 where ID NOT regexp '^[0-9]+$'; 
+
 -- Returns empty if there are no blank spaces or null values --
 select * from 2018starter
 where ID is null OR ID = ''; 
@@ -31,7 +33,8 @@ where name is null OR name = '';
 
 -- C. Incorrect Values in main_category column --
 -- check all the distinct maincategories present in the column to detect any incorrect terms--
-select distinct(main_category) from 2018starter; 
+select distinct(main_category) from 2018starter;
+ 
 -- will return values with this values as they are currency values and returns empty once fixed --
 select * from `2018starter`
 where main_category in  ('USD', 'GBP'); 
@@ -39,56 +42,79 @@ where main_category in  ('USD', 'GBP');
 -- D. Incorrect Values in currency column --
 -- returns empty if there are no blank spaces or null values --
 select* from `2018starter` where currency is null or currency = ''; 
+
  -- check all the distinct currency present in the column to detect any incorrect values--
 select distinct(currency) from 2018starter;
+
 -- returns the details for the currency column with the date values and will return empty once fixed--
 select * from `2018starter` 
-where currency in ('4/6/2015', '5/12/2015', '3/8/2015'); 
+where currency in ('2015-04-06', '2015-05-12', '2015-03-08'); 
 
 -- E. Incorrect Values in goal column --
 -- returns empty if there are no blank spaces or null values --
 select* from `2018starter` where goal is null or goal = ''; 
+
 -- returns values in this column that is not a number format and returns empty once fixed--
 select * from `2018starter`
 where goal NOT REGEXP '^[0-9]+(\.[0-9]+)?$';
 
+-- F. Incorrcet Values in Deadline --
+-- returns empty if there are no blank spaces or null values --
+select* from `2018starter` where deadline is null or deadline = ''; 
+
+-- returns values that are not dates regardless of the date format used and returns empty once fixed --
+select * from 2018starter
+where deadline not regexp '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+  and deadline not regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4}$'
+  and deadline not regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4}$'
+  and deadline not regexp '^[0-9]{4}/[0-9]{2}/[0-9]{2}$';
+
+
 -- F. Incorrcet Values in launched --
 -- returns empty if there are no blank spaces or null values --
 select* from `2018starter` where launched is null or launched = ''; 
+
 -- returns values that are not dates regardless of the date format used and returns empty once fixed --
-select * from `2018starter`
-where STR_TO_DATE(launched, '%Y-%m-%d') is null
-   and STR_TO_DATE(launched, '%m/%d/%Y') is null
-   and STR_TO_DATE(launched, '%d-%m-%Y') is null
-   and STR_TO_DATE(launched, '%Y/%m/%d') is null;
+select * from 2018starter
+where launched not regexp '^[0-9]{4}-[0-9]{2}-[0-9]{2}( [0-9]{2}:[0-9]{2}(:[0-9]{2})?)?$'
+  and launched not regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4}( [0-9]{2}:[0-9]{2}(:[0-9]{2})?)?$'
+  and launched not regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4}( [0-9]{2}:[0-9]{2}(:[0-9]{2})?)?$'
+  and launched not regexp '^[0-9]{4}/[0-9]{2}/[0-9]{2}( [0-9]{2}:[0-9]{2}(:[0-9]{2})?)?$';
+
 
 -- G. Incorrect Values in pledged column --
 -- returns empty if there are no blank spaces or null values --
 select* from `2018starter` where pledged is null or pledged = ''; 
+
  -- returns values in this column that is not a number format and returns empty once fixed--
 select * from `2018starter`
 where pledged not regexp '^[0-9]+(\.[0-9]+)?$';
 
--- H. IncorreCt Values in State Column --
+-- H. Incorrect Values in State Column --
 -- Returns empty if there are no blank spaces or null values --
 select * from 2018starter where state is null OR state = '';
+
 -- check all the distinct states present in the column to detect any incorrect terms-- 
 select distinct(state) from 2018starter; 
+
  -- selects and shows the incorrect values in state column, returns empty once fixed --
 select * from 2018starter where state regexp '^[0-9]+(\.[0-9]+)?$';
 
 -- I. Incorrect Values in backers Column --
 -- Returns empty if there are no blank spaces or null values --
 select * from 2018starter where backers is null OR backers = '';
- -- returns values in this column that is not a number format and returns empty once fixed--
+
+ -- returns values in this backers column that is not a number format and returns empty once fixed--
 select * from `2018starter`
 where backers not regexp '^[0-9]+(\.[0-9]+)?$';
 
 -- J. Incorrect Values in country Column --
 -- check all the distinct countries present in the column to detect any incorrect terms-- 
 select distinct(country) from 2018starter; 
+
 -- Returns empty if there are no blank spaces or null values --
 select * from 2018starter where country is null OR country = '';
+
 -- checks for incorrect values and returns empty once fixed --
 select * from 2018starter where country regexp '^[0-9]+(\.[0-9]+)?$';
 
@@ -101,6 +127,7 @@ where `usd pledged` not regexp '^[0-9]+(\.[0-9]+)?$';
 -- K. Incorrect Values in usd pledged real Column --
 -- Returns null values or blank spaces, and returns empty once fixed --
 select* from `2018starter` where usd_pledged_real is null or usd_pledged_real = ''; 
+
 -- checks for incorrect values and returns empty once fixed --
 select * from `2018starter`
 where usd_pledged_real not regexp '^[0-9]+(\.[0-9]+)?$';
@@ -108,13 +135,14 @@ where usd_pledged_real not regexp '^[0-9]+(\.[0-9]+)?$';
 -- L. Incorrect Values in usd pledged goal Column --
 -- returns empty once fixed --
 select* from `2018starter` where usd_goal_real is null or usd_goal_real = '';
+
 -- checks for incorrect values and returns empty once fixed --
 select * from `2018starter`
 where usd_goal_real not regexp '^[0-9]+(\.[0-9]+)?$';
 
 -- 4. From the checks above, values in column with ID 1105671266, 2054476698, 96887393, are the ones with incorrect values -- 
 -- FIXING the incorrect values for the three rows with ID 1105671266, 2054476698, 96887393 --
-set SQL_SAFE_UPDATES = 1;
+set SQL_SAFE_UPDATES = 0;
 -- For ID = 1105671266
 select
     main_category, currency, deadline, goal, launched, pledged,
@@ -213,30 +241,57 @@ set SQL_SAFE_UPDATES = 1;
 
 -- 6. Fix deadline and launched date format  to a standardized format--
 -- Standardizing the date format for deadline column --
-set SQL_SAFE_UPDATES = 0;
-update 2018starter
-set deadline = case 
-    when str_to_date(deadline, '%m/%d/%Y') is not null then date_format(str_to_date(deadline, '%m/%d/%Y'), '%Y/%m/%d')
-    when str_to_date(deadline, '%d/%m/%Y') is not null then date_format(str_to_date(deadline, '%d/%m/%Y'), '%Y/%m/%d')
-    when str_to_date(deadline, '%d-%m-%Y') is not null then date_format(str_to_date(deadline, '%d-%m-%Y'), '%Y/%m/%d')
-    when str_to_date(deadline, '%Y-%m-%d') is not null then date_format(str_to_date(deadline, '%Y-%m-%d'), '%Y/%m/%d')
-    else deadline -- fallback: keep the original value if format is unrecognized
-end;
-set SQL_SAFE_UPDATES = 1;
-select deadline from 2018starter;
+set sql_safe_updates = 0;
 
--- 7. Standardizing the date format for launched column --
-set SQL_SAFE_UPDATES = 0;
 update 2018starter
 set launched = case
-when str_to_date(launched, '%m/%d/%Y %H:%i') is not null then date_format(str_to_date(launched, '%m/%d/%Y %H:%i'), '%Y/%m/%d %H:%i')
-when str_to_date(launched, '%m-%d-%Y %H:%i') is not null then date_format(str_to_date(launched, '%m-%d-%Y %H:%i'), '%Y/%m/%d %H:%i')
-when str_to_date(launched, '%d/%m/%Y %H:%i') is not null then date_format(str_to_date(launched, '%d/%m/%Y %H:%i'), '%Y/%m/%d %H:%i')
-when str_to_date(launched, '%d-%m-%Y %H:%i') is not null then date_format(str_to_date(launched, '%d-%m-%Y %H:%i'), '%Y/%m/%d %H:%i')
-when str_to_date(launched, '%Y-%m-%d %H:%i') is not null then date_format(str_to_date(launched, '%Y-%m-%d %H:%i'), '%Y/%m/%d %H:%i')
-else launched -- fallback: keep the original value if format is unrecognized
+    -- handles formats like: 12/31/2020 14:30:59 or 12/31/2020 14:30
+    when launched regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}(:[0-9]{2})?$' 
+    then date_format(str_to_date(launched, '%m/%d/%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+
+    -- handles formats like: 31-12-2020 09:15:05 or 31-12-2020 09:15
+    when launched regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}(:[0-9]{2})?$'
+        then date_format(str_to_date(launched, '%d-%m-%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+
+    -- handles formats like: 2020-12-31 12:00:00 or 2020-12-31 12:00
+    when launched regexp '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}(:[0-9]{2})?$'
+        then date_format(str_to_date(launched, '%Y-%m-%d %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+
+    else launched
 end;
-set SQL_SAFE_UPDATES = 1;
+
+set sql_safe_updates = 1;
+
+
+-- 7. Standardizing the date format for launched column --
+set sql_safe_updates = 0;
+update 2018starter
+set launched = case
+    -- Format: MM/DD/YYYY HH:MM:SS
+    when launched regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%m/%d/%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+    -- Format: MM/DD/YYYY HH:MM
+    when launched regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%m/%d/%Y %H:%i'), '%Y-%m-%d %H:%i:00')
+    -- Format: DD/MM/YYYY HH:MM:SS
+    when launched regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%d/%m/%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+    -- Format: DD/MM/YYYY HH:MM
+    when launched regexp '^[0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%d/%m/%Y %H:%i'), '%Y-%m-%d %H:%i:00')
+    -- Format: MM-DD-YYYY HH:MM:SS
+    when launched regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%m-%d-%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+    -- Format: MM-DD-YYYY HH:MM
+    when launched regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%m-%d-%Y %H:%i'), '%Y-%m-%d %H:%i:00')
+    -- Format: DD-MM-YYYY HH:MM:SS
+    when launched regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%d-%m-%Y %H:%i:%s'), '%Y-%m-%d %H:%i:%s')
+    -- Format: DD-MM-YYYY HH:MM
+    when launched regexp '^[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}$' then date_format(str_to_date(launched, '%d-%m-%Y %H:%i'), '%Y-%m-%d %H:%i:00')
+    -- Format: YYYY-MM-DD HH:MM:SS (already good)
+    when launched regexp '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$' then launched
+    -- Format: YYYY-MM-DD HH:MM
+    when launched regexp '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$' then concat(launched, ':00')
+    -- Anything else (unrecognized format)
+    else null
+end;
+set sql_safe_updates = 1;
+
 select launched from 2018starter;
 
 -- 8. Fixing the data type  for each column in 2018starter table to ensure quality and consistency--
@@ -285,25 +340,7 @@ update 2018starter
 set projectdurationDays = datediff(deadline, launchDate);
 set SQL_SAFE_UPDATES = 1;
 
--- Finds the values where the deadline is less than the launched Date --
-select * from 2018starter 
-where deadline < launchDate;
-
--- Fixing the incorrect deadline and launched values.
-select deadline, launched, launchDate into @tempdeadline, @templaunched, @templaunchDate
-from 2018starter
-where ID = 1000002330;
-set SQL_SAFE_UPDATES = 0;
-update 2018starter
-set deadline = @templaunchDate,
-launchDate = @tempdeadline,
-launched = concat(@templaunchDate, ' ', @templaunchTime)
-where ID = 1000002330;
-set SQL_SAFE_UPDATES = 1;
-
--- to check the fix made to the incorrect launched and deadline --
-select * from 2018starter 
-where ID = 1000002330;
-
 select* from 2018starter; 
+
+
 
