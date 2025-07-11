879e86606cc7ada349aba1584860a40bee330b3e
Evelyn Bankz <113942245+EvelynBankz@users.noreply.github.com>
2025-07-11 11:37:54 +0100

    Update Predictive_Analysis_Model.sql

--- KickstarterProject/PredictiveAnalysisModel.sql
+++ KickstarterProject/PredictiveAnalysisModel.sql
@@ -13,7 +13,7 @@ group by country
 order by total_projects DESC;
 
 
--- Total projects, Successprojects, Failed Projects, and Success and Failure rate by category --
+-- Total projects, Successprojects, Failed Projects, and Success and Failure rate by main_category --
 SELECT 
   main_category,
   count(*) as total_projects,
@@ -26,39 +26,93 @@ from 2018starter
 group by main_category
 order by total_projects DESC;
 
+-- Total projects, Successprojects, Failed Projects, and Success and Failure rate by launchmonthin maincategory -- 
+select 
+  main_category,
+  launchyear,
+  launch_month_name,
+  count(*) as total_projects,
+  sum(case when state = 'successful' then 1 else 0 end) as successful_projects,
+  sum(case when state = 'failed' then 1 else 0 end) as failed_projects,
+  round(100.0 * sum(case when state = 'successful' then 1 else 0 end) / count(*), 0) as success_rate,
+  round(100.0 * sum(case when state = 'failed' then 1 else 0 end) / count(*), 0) as failure_rate
+from 2018starter
+group by main_category, launchyear, launch_month_name
+order by 
+  main_category,
+  launchyear,
+  field(launch_month_name, 'January','February','March','April','May','June','July','August','September','October','November','December');
+
+
+-- Total projects, Successprojects, Failed Projects, and Success and Failure rate by launchmonth in Goal levels --
+select 
+  -- Goal range amount
+  case
+    when goal <= 1000 then '0 - 1000'
+    when goal > 1000 and goal <= 5000 then '1001 - 5000'
+    when goal > 5000 and goal <= 10000 then '5001 - 10000'
+    when goal > 10000 and goal <= 50000 then '10001 - 50000'
+    else '50001 and above'
+  end as goal_range_amount,
+
+  -- Goal level name
+  case
+    when goal <= 1000 then 'Very Low Goal'
+    when goal > 1000 and goal <= 5000 then 'Low Goal'
+    when goal > 5000 and goal <= 10000 then 'Medium Goal'
+    when goal > 10000 and goal <= 50000 then 'High Goal'
+    else 'Very High Goal'
+  end as goal_level,
+
+  launchyear,
+  launch_month_name,
+  count(*) as total_projects,
+  sum(case when state = 'successful' then 1 else 0 end) as successful_projects,
+  sum(case when state = 'failed' then 1 else 0 end) as failed_projects,
+  round(100.0 * sum(case when state = 'successful' then 1 else 0 end) / count(*), 0) as success_rate,
+  round(100.0 * sum(case when state = 'failed' then 1 else 0 end) / count(*), 0) as failure_rate
+
+from 2018starter
+group by goal_range_amount, goal_level, launchyear, launch_month_name
+order by 
+  goal_level,
+  launchyear,
+  field(launch_month_name, 'January','February','March','April','May','June','July','August','September','October','November','December');
+
 
 -- Success and Failure rate by Goal ranges --
-select 
-GoalRangeAmount, GoalRange, 
-count(*) as TotalProjects,
- sum(case when state = 'successful' then 1 else 0 end) as successful_projects,
-  sum(case when state = 'failed' then 1 else 0 END) as failed_projects,
+select  GoalRangeAmount, GoalRange, 
+  count(*) as TotalProjects,
+  sum(case when state = 'successful' then 1 else 0 end) as successful_projects,
+  sum(case when state = 'failed' then 1 else 0 end) as failed_projects,
   round(100.0 * sum(case when state = 'successful' then 1 else 0 end) / count(*), 0) as SuccessRate, 
-  round(100.0* SUM(case when state = 'failed' then 1 else 0 end)/ count(*), 0) as FailureRate
-from ( 
-select goal, state,
-   case
-       when goal <= 1000 then '0 - 1000'
-       when goal >1000 and goal <= 5000 then '1001 - 5000'
-       when goal > 5000 and goal <= 10000 then '50001 - 10000'
-       when goal > 10000 and goal <= 50000 then '10001 - 50,000'
-       else '500001 and above' end as GoalRangeAmount,
-
-   case
-       when goal <= 1000 then 'Very Low Goal'
-       when goal >1000 and goal <= 5000 then 'Low Goal'
-       when goal > 5000 and goal <= 10000 then 'Medium Goal'
-       when goal > 10000 and goal <= 50000 then 'High Goal'
-       else 'Very High Goal' end as GoalRange
-from 2018starter) as subquery
-group by  GoalRangeAmount, GoalRange
-order by GoalRange;
-
-
-
-
-
-
+  round(100.0 * sum(case when state = 'failed' then 1 else 0 end) / count(*), 0) as FailureRate
+from (
+  select goal, state,
+    case
+      when goal <= 1000 then '0 - 1000'
+      when goal > 1000 and goal <= 5000 then '1001 - 5000'
+      when goal > 5000 and goal <= 10000 then '5001 - 10000'
+      when goal > 10000 and goal <= 50000 then '10001 - 50,000'
+      else '50001 and above' end as GoalRangeAmount,
+    
+    case
+      when goal <= 1000 then 'Very Low Goal'
+      when goal > 1000 and goal <= 5000 then 'Low Goal'
+      when goal > 5000 and goal <= 10000 then 'Medium Goal'
+      when goal > 10000 and goal <= 50000 then 'High Goal'
+      else 'Very High Goal' end as GoalRange,
+  
+    case
+      when goal <= 1000 then 1
+      when goal > 1000 and goal <= 5000 then 2
+      when goal > 5000 and goal <= 10000 then 3
+      when goal > 10000 and goal <= 50000 then 4
+      else 5 end as goal_range_order
+    
+  from 2018starter) as subquery
+group by goal_range_order, GoalRangeAmount, GoalRange
+order by goal_range_order;
 
 
 -- Find AVERAGE Goal set for all successful projects and all failed projects -- 
@@ -80,13 +134,37 @@ where state = 'failed';
 
 -- Will Succeed Yes/No Outcome --
 -- Model --
-select id, name, goal, backers, pledged,
+select id, name, goal, backers, pledged, state,
 case when
-     (case when goal <= 10163 then 1 else 0 end + 
-     case when backers >= 264 then 1 else 0 end + 
-     case when pledged >= 67347 then 1 else 0 end +
-     case when pledged >= goal then 1 else 0 end) >=1
+     goal <= 10163 and backers > 1 and pledged >= goal
     then 'likely to Succeed'
     else 'Not likely to Succeed'
     end as "Succeed Prediction"
 from 2018starter;
+
+-- From Maincategory --
+ select main_category, count(*) as total_projects,
+  sum(case when succeed_prediction = 'likely to Succeed' then 1 else 0 end) as likely_success,
+  sum(case when succeed_prediction = 'Not likely to Succeed' then 1 else 0 end) as likely_fail
+from (
+  select *, case 
+      when goal <= 10163 and backers > 1 and pledged >= goal 
+      then 'likely to Succeed' else 'Not likely to Succeed'
+    end as succeed_prediction 
+    from 2018starter) as prediction
+group by main_category
+order by likely_success desc;
+
+-- From Country --
+select country, count(*) as total_projects,
+  sum(case when succeed_prediction = 'likely to Succeed' then 1 else 0 end) as likely_success,
+  sum(case when succeed_prediction = 'Not likely to Succeed' then 1 else 0 end) as likely_fail
+from (
+  select *, case 
+      when goal <= 10163 and backers > 1 and pledged >= goal 
+      then 'likely to Succeed' else 'Not likely to Succeed'
+    end as succeed_prediction 
+    from 2018starter) as prediction
+group by country
+order by likely_success desc;
+
